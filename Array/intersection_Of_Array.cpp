d704d2bc868454fa4cabea3a6dc68a331ca3e343
Abhay Yadav <yadavabhay966@gmail.com>
2024-02-01 15:24:44 +0530

    Intersection of array Done!

---
+++ Array/intersection_Of_Array.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    vector <int> arr{1,3,5,8,0};
+    vector <int> brr{3,5,6,7,9};
+
+    // ans wala array
+    vector <int> ans;
+
+    for( int i = 0; i < arr.size(); i++ ){
+        for( int j = 0; j < brr.size(); j++ ){
+            if( arr[i] == brr[j] ){
+                ans.push_back(arr[i]);
+            }
+        }
+    }
+
+    // printing ans wala array
+    for( int k = 0; k < ans.size(); k++ ){
+        cout<<ans[k]<<" ";
+    }
+
+    return 0;
+}
