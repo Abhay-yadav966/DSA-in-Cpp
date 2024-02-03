6865126fbe6e382900bce96cdea736f92033c1bb
Abhay Yadav <yadavabhay966@gmail.com>
2024-02-03 22:56:34 +0530

    Move all -ve no. to left qn done!

---
+++ Array/move_All_Negative_number_To_Left.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    vector <int> arr{1, 2, -3, 4, -5, 6};
+    
+    int start = 0;
+    int i = 0;
+    int end = arr.size() - 1;
+
+    while( start <= end ){
+        if( arr[start] < 0 ){
+            swap( arr[i] , arr[start] );
+            start++;
+            i++;
+        }
+        else{
+            swap(arr[start], arr[end]);
+            end--;
+        }
+    }
+
+    for( int i = 0; i < arr.size(); i++ ){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
