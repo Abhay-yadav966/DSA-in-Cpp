f6f80f1f4a31f3fb6fc6a8a86e84b79536e47ea5
Abhay Yadav <yadavabhay966@gmail.com>
2024-09-16 17:18:28 +0530

    ADD: brute Force Approach

--- Backtracking/Max_SubArray_Sum.cpp
+++ Backtracking/Max_SubArray_Sum.cpp
@@ -3,6 +3,8 @@
 #include<limits.h>
 using namespace std;
 
+// using looping - Brute Approch
+
 int main(){
 
     vector <int> arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
@@ -24,6 +26,48 @@ int main(){
     }
 
     cout<<"The maximum of the subarray : "<<maxi<<endl;
+
+    return 0;
+}
+
+// Recursive - Brute Approch
+
+void findMaxSubArray( vector <int> arr, int start, int& maxi){
+
+    // base case
+    if( start >= arr.size() ){
+        return;
+    }
+
+    cout<<"Start : "<<start<<endl;
+    cout<<maxi<<endl;
+
+
+    for( int j = start + 1; j < arr.size(); j++ ){
+        int sum = 0;
+        for( int k = start; k <= j; k++ ){
+            sum = sum + arr[k];
+        }
+        if( sum > maxi ){
+            maxi = sum;
+        }
+    }
+
+    findMaxSubArray( arr, start+1, maxi);
+
+}
+
+int main(){
+
+    vector <int> arr = {-2,1,-3,4,-1,2,1,-5,4};
+
+    int start = 0;
+    int ans = 0;
+    int maxi = INT_MIN;
+
+    findMaxSubArray( arr, start, maxi);
+
+    cout<<"Ans : "<<maxi<<endl;
 
     return 0;
 }
